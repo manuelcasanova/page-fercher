94e64bd0dc153b384ac91a5bb6d4ec0a5735ba98
Manuel Casanova <manucasanova@hotmail.com>
2022-02-23 09:57:13 -0800

    LINTED AND STATS.SIZE ADDED

--- fetcher.js
+++ fetcher.js
@@ -6,29 +6,24 @@ const webAddress = args[0];
 const localPath = args[1];
 
 
-
 request(webAddress, (error, response, body) => {
   const data = body;
 
 
-
-//fs.writeFile(file, data, options, callback);
-fs.writeFile("./index.html", data, {encoding: "utf8"},
- error => {
-   if (error) {
-     console.log(error)
-   } else {
-     console.log("File written");
-   }
- });
+  //fs.writeFile(file, data, options, callback);
+  fs.writeFile(localPath, data, error => {
+    if (error) {
+      console.log(error);
+    }
+  });
 
 
-//fs.stat(path, options, callback);
-fs.stat(localPath, (error, stats) => {
-  if (!error) {
-    console.log(`Downloaded and saved ${stats.blksize} bytes to ${localPath}`);
-  }
-});
+  //fs.stat(path, options, callback);
+  fs.stat(localPath, (error, stats) => {
+    if (!error) {
+      console.log(`Downloaded and saved ${stats.size} bytes to ${localPath}`);
+    }
+  });
 
 
 
