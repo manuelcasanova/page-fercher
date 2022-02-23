51acc9fd7cd7f97652600f2ab2bf179dd2a81866
Manuel Casanova <manucasanova@hotmail.com>
2022-02-23 09:23:24 -0800

    REQUESTS THE DATA, WRITES THE DATA ON A NEW FILE RETURNS THE REQUIRED STRING

--- fetcher.js
+++ fetcher.js
@@ -1,3 +1,52 @@
-let args = process.argv.slice(2)
+let args = process.argv.slice(2);
 
 const request = require('request');
+const fs = require('fs');
+const webAddress = args[0];
+const localPath = args[1];
+
+
+
+request(webAddress, (error, response, body) => {
+  const data = body;
+
+
+
+//fs.writeFile(file, data, options, callback);
+fs.writeFile("./index.html", data, {encoding: "utf8"},
+ error => {
+   if (error) {
+     console.log(error)
+   } else {
+     console.log("File written");
+   }
+ });
+
+
+//fs.stat(path, options, callback);
+fs.stat(localPath, (error, stats) => {
+  if (!error) {
+    console.log(`Downloaded and saved ${stats.blksize} bytes to ${localPath}`);
+  }
+});
+
+
+
+}); //closing of function starting in line 10
+
+
+
+
+
+
+
+
+
+
+// console.log(webAddress, localPath);
+
+
+
+
+
+
